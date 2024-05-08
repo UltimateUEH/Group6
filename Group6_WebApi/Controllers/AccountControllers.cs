1c295d56decf92155613237f0ea37fe46428fd3e
VoHoangTu <tuvo.31211027623@st.ueh.edu.vn>
2024-05-08 16:45:55 +0700

    Tạo API cho LOGIN/REGISTER
    Xử lý kiểm tra dữ liệu - thêm tài khoản

---
+++ Group6_WebApi/Controllers/AccountControllers.cs
@@ -0,0 +1,111 @@
+﻿using Group6_WebApi.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Group6_WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly Group06Context _context;
+
+        public LoginController(Group06Context context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Account login)
+        {
+            // Hash the provided password
+            string hashedPassword = HashPassword(login.Password);
+
+            // Find user by username and hashed password
+            var user = _context.Accounts.FirstOrDefault(u => u.Email == login.Email && u.Password == hashedPassword);
+
+            if (user != null)
+            {
+                return Ok("Login successful!");
+            }
+            else
+            {
+                return BadRequest("Invalid credentials");
+            }
+        }
+
+        private string HashPassword(string password)
+        {
+            using (var sha256 = SHA256.Create())
+            {
+                // Compute hash of the password
+                byte[] bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+
+                // Convert byte array to a string representation
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
+    }
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegisterController : ControllerBase
+    {
+        private readonly Group06Context _context;
+
+        public RegisterController(Group06Context context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public IActionResult Register(Account registration)
+        {
+            // Kiểm tra xem tên người dùng đã tồn tại chưa
+            if (_context.Accounts.Any(u => u.Email == registration.Email))
+            {
+                return BadRequest("Email already exists");
+            }
+
+            // Hash the password
+            registration.Password = HashPassword(registration.Password);
+
+            // Tạo một bản ghi mới và thêm vào cơ sở dữ liệu
+            _context.Accounts.Add(registration);
+            _context.SaveChanges();
+
+            return Ok("");
+        }
+        private string HashPassword(string password)
+        {
+            using (var sha256 = SHA256.Create())
+            {
+                // Compute hash of the password
+                byte[] bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+
+                // Convert byte array to a string representation
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
+    }
+
+    [AllowAnonymous, HttpGet("forgot-password")]
+    public async IActionResult ForgotPassword()
+    {
+        return View();
+    }
+}
---
+++ Group6_WebApi/Models/ForgotPassword.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace Group6_WebApi.Models
+{
+    public class PasswordResetToken
+    {
+        [Required, EmailAddress, Display(Name = "Registered email address")]
+        public string Email { get; set; }
+        public bool EmailConfirmed { get; set; }
+    }
+}
